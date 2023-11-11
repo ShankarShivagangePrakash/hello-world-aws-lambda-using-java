31dc0445e0e478d25abf403dccad8731a233ad96
sshivagangeprakash <Shankar.ShivagangePrakash@logmein.com>
2023-11-11 13:05:14 +0530

    data types: boolean

--- HelloWorldFunction/src/main/java/helloworld/DataTypes.java
+++ HelloWorldFunction/src/main/java/helloworld/DataTypes.java
@@ -5,4 +5,7 @@ public class DataTypes {
     public int getNumber(float number) {
         return (int)number;
     }
+    public boolean getBoolean(float number) {
+        return number > 100 ? true : false;
+    }
 }
