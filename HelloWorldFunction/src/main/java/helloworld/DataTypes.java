60224d67d375b417507d6249f383e3e33f9c0cba
sshivagangeprakash <Shankar.ShivagangePrakash@logmein.com>
2023-11-11 13:10:58 +0530

    data types: list

--- HelloWorldFunction/src/main/java/helloworld/DataTypes.java
+++ HelloWorldFunction/src/main/java/helloworld/DataTypes.java
@@ -1,5 +1,10 @@
 package helloworld;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class DataTypes {
 
     public int getNumber(float number) {
@@ -7,5 +12,20 @@ public class DataTypes {
     }
     public boolean getBoolean(float number) {
         return number > 100 ? true : false;
+    }
+
+    public List<Integer> getStudentsScore(List<String> names) {
+
+        Map<String, Integer> studentsScore = new HashMap<>();
+        studentsScore.put("gill", 100);
+        studentsScore.put("Harris", 95);
+        studentsScore.put("Bob", 85);
+
+        List<Integer> matchingScores = new LinkedList<>();
+        names.forEach(name -> {
+            matchingScores.add(studentsScore.get(name));
+        });
+
+        return matchingScores;
     }
 }
